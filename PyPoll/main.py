7b68bff62f8cfe62f3cb2a9c1936b5cbe4e8d0e6
Rachael Songalewski <44907804+rsongalewski@users.noreply.github.com>
2020-07-07 23:36:30 -0400

    Python Homework Upload

---
+++ PyPoll/main.py
@@ -0,0 +1,67 @@
+# Import modules os and csv. Importing numpy in order to use the unique function
+import os
+import csv
+
+# Set the path for the CSV file
+
+PyPoll_data = os.path.join("Resources","election_data.csv")
+
+# Create lists to store data
+
+count = 0
+candidatelist = []
+unique_candidate = []
+vote_count = []
+vote_percent = []
+
+# Open the CSV
+
+with open(PyPoll_data, newline="") as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+    csv_header = next(csvreader)
+    # Conduct ask
+    for row in csvreader:
+        # Count total number of votes
+        count = count + 1
+        # Set candidate names to candidatelist
+        candidatelist.append(row[2])
+        # Create a set from the candidatelist to get the unique candidate names
+    for x in set(candidatelist):
+        unique_candidate.append(x)
+        # y is the total number of votes per candidate
+        y = candidatelist.count(x)
+        vote_count.append(y)
+        # z is the percent of total votes per candidate
+        z = (y/count)*100
+        vote_percent.append(z)
+        
+    winning_vote_count = max(vote_count)
+    winner = unique_candidate[vote_count.index(winning_vote_count)]
+    
+# Print to terminal
+# Output perhaps needs to be rounded to 3 decimal points. Leaving that formatting out for now) 
+ 
+print("-------------------------")
+print("Election Results")   
+print("-------------------------")
+print("Total Votes :" + str(count))    
+print("-------------------------")
+for i in range(len(unique_candidate)):
+            print(unique_candidate[i] + ": " + str(vote_percent[i]) +"% (" + str(vote_count[i])+ ")")
+print("-------------------------")
+print("The winner is: " + winner)
+print("-------------------------")
+
+# Print to a text file: election_results.txt
+# Output perhaps needs to be rounded to 3 decimal points. Leaving that formatting out for now) 
+
+with open('election_results.txt', 'w') as text:
+    text.write("Election Results\n")
+    text.write("---------------------------------------\n")
+    text.write("Total Vote: " + str(count) + "\n")
+    text.write("---------------------------------------\n")
+    for i in range(len(set(unique_candidate))):
+        text.write(unique_candidate[i] + ": " + str(vote_percent[i]) +"% (" + str(vote_count[i]) + ")\n")
+    text.write("---------------------------------------\n")
+    text.write("The winner is: " + winner + "\n")
+    text.write("---------------------------------------\n")
